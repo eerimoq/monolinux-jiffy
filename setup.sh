bfbe997352e72aa98797b8d6b63bca031ffaa319
Erik Moqvist <erik.moqvist@gmail.com>
2019-05-28 07:44:32 +0200

    New Monolinux.

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ export ML_LINUX_CONFIG=$(readlink -f app/linux-4.14.78-jiffy.config)
 export ML_SOURCES=$(readlink -f sources)
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-musleabi-
-export ML_AUTOTOOLS_HOST=--host=arm-linux-musleabi
+export ML_AUTOTOOLS_HOST=arm-linux-musleabi
 export PATH=$PATH:$(readlink -f arm-linux-musleabi-cross/bin)
